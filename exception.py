5946fe7db7c5a4ca7a4f551e0e030c01f6da6a34
Дарья Зайцева <daria7ztv@gmail.com>
2022-02-23 19:25:46 +0300

    7.5

--- exception.py
+++ exception.py
@@ -6,3 +6,12 @@ class URLNotResponding(Exception):
     
     def __str__(self):
         return self.message
+
+class EmptyData(Exception):
+    """Исключение в случае недоступности URL"""
+    def __init__(self, value):
+        self.message = f'Нет данных в ответе - {value}'
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return self.message
--- homework.py
+++ homework.py
@@ -1,19 +1,19 @@
 import os
-import logging
 import time
 import sys
-
-import requests
-
-from dotenv import load_dotenv
-import telegram
+import logging
 from logging import StreamHandler
 from http import HTTPStatus
 
-from exception import URLNotResponding
+import requests
+from dotenv import load_dotenv
+import telegram
+from json.decoder import JSONDecodeError
+
+from exception import URLNotResponding, EmptyData
+
 
 load_dotenv()
-
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -44,57 +44,63 @@ def send_message(bot, message):
     """Отправляет сообщение пользователю."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-    except Exception as error:
-        logger.error(f'Сбой при отправке сообщения - {error}')
+        logger.info(f'Бот отправил сообщение - {message}')
+    except telegram.TelegramError as error:
+        raise telegram.TelegramError(f'Не удалось отправить сообщение - {error}')
 
 
 def get_api_answer(current_timestamp):
     """Получает ответ от эндпоинта."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    if requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params=params
-    ).status_code != HTTPStatus.OK:
-        raise URLNotResponding(ENDPOINT)
-    else:
+    try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        return response.json()
+        if response.status_code != HTTPStatus.OK:
+            raise URLNotResponding(ENDPOINT)
+        response = response.json()
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+    except JSONDecodeError:
+        raise JSONDecodeError('Ответ не преобразуется в json.')
+    except ValueError as error:
+        raise ValueError(f'Ответ не в формате json - {error}')
+    except AttributeError as error:
+        raise AttributeError(f'Ответ не имеет аттрибута json - {error}')
+    else:
+        return response
 
 
 def check_response(response):
     """Получает список всех домашних работ."""
-    JsonKey = 'homeworks'
+    json_key = 'homeworks'
     if response is None:
-        raise Exception(f'Нет данных в ответе - {response}')
+        raise EmptyData(response)
     elif not isinstance(response, dict):
-        raise TypeError
+        raise TypeError('Тип ответа не dict.')
     else:
-        homeworks = response.get(JsonKey)
+        homeworks = response.get(json_key)
         if homeworks is None:
-            raise KeyError
+            raise KeyError(f'По ключу {json_key} нет данных.')
         elif not isinstance(homeworks, list):
-            raise AssertionError(f'Ответ по ключу {JsonKey} не в типе list.')
+            raise AssertionError(f'Ответ по ключу {json_key} не в типе list.')
         return homeworks
 
 
 def parse_status(homework):
     """Получает статус последней домашней работы."""
-    JsonKey = 'homework_name'
-    homework_name = homework.get(JsonKey)
+    json_key = 'homework_name'
+    homework_name = homework.get(json_key)
     if homework_name is None:
-        raise KeyError
-    JsonKey = 'status'
-    homework_status = homework.get(JsonKey)
+        raise KeyError(f'По ключу {json_key} нет данных.')
+    json_key = 'status'
+    homework_status = homework.get(json_key)
     if homework_status is None:
-        raise KeyError
-
-    if homework_status in HOMEWORK_STATUSES.keys():
+        raise KeyError(f'По ключу {json_key} нет данных.')
+    if homework_status in HOMEWORK_STATUSES:
         verdict = HOMEWORK_STATUSES[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
     else:
-        raise KeyError
+        raise KeyError(f'По ключу {homework_status} нет данных.')
 
 
 def check_tokens():
@@ -114,8 +120,8 @@ def check_tokens():
 # flake8: noqa: C901
 def main():
     """Основная логика работы бота."""
-    CheckConstantsFlag = check_tokens()
-    if CheckConstantsFlag is False:
+    check_constants_flag = check_tokens()
+    if not check_constants_flag:
         return
 
     try:
@@ -125,71 +131,52 @@ def main():
         return
 
     current_timestamp = int(time.time())
-    ContinueFlag: bool = True
+    continue_flag: bool = True
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
-            if response.get('code') == 'not_authenticated':
-                message = 'Учетные данные неверные (PRACTICUM_TOKEN)'
-                logger.error(message)
-                send_message(bot, message)
-                return
-            elif response.get('code') == 'UnknownError':
-                message = 'Неверные ключи для доступа к эндпойнту'
-                logger.error(message)
-                send_message(bot, message)
-                return
             homeworks = check_response(response)
             if homeworks:
                 homework = homeworks[0]
                 message = parse_status(homework)
                 send_message(bot, message)
-                logger.info(f'Бот отправил сообщение - {message}')
             else:
                 logger.debug('Отсутствие в ответе новых статусов')
-            ContinueFlag = True
+            continue_flag = True
 
             current_timestamp = int(time.time())
-            time.sleep(RETRY_TIME)
 
         except URLNotResponding as error:
             logger.error(error.message)
-            if ContinueFlag is True:
+            if continue_flag:
                 send_message(bot, error.message)
-            ContinueFlag = False
-            time.sleep(RETRY_TIME)
+        
+        except EmptyData as error:
+            logger.error(error.message)
+            if continue_flag:
+                send_message(bot, error.message)
 
-        except AssertionError:
-            logger.error('Неверный тип данных')
-            if ContinueFlag is True:
-                send_message(bot, 'Неверный тип данных')
-            ContinueFlag = False
-            time.sleep(RETRY_TIME)
-
-        except TypeError:
-            logger.error('Неверный тип данных')
-            if ContinueFlag is True:
-                send_message(bot, 'Неверный тип данных')
-            ContinueFlag = False
-            time.sleep(RETRY_TIME)
-
-        except KeyError:
-            print(KeyError)
-            logger.error('Ключ отсутсвует')
-            if ContinueFlag is True:
-                send_message(bot, 'Ключ отсутсвует')
-            ContinueFlag = False
-            time.sleep(RETRY_TIME)
+        except SystemExit as e:
+            error = f'URL недоступно - {e.message}'
+            logger.error(error)
+            if continue_flag:
+                send_message(bot, error)
+        
+        except JSONDecodeError as e:
+            error = e.message
+            logger.error(error)
+            if continue_flag:
+                send_message(bot, error)
 
         except Exception as error:
             logger.error(f'Сбой в работе программы: {error}')
-            if ContinueFlag is True:
+            if continue_flag:
                 send_message(bot, f'Сбой в работе программы: {error}')
-            ContinueFlag = False
+        
+        finally:
             time.sleep(RETRY_TIME)
-        else:
-            pass
+            continue_flag = False
 
 
 if __name__ == '__main__':
